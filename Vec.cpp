b5e9a3061289fd7202fe7d76b754d9e145e6f625
Gilbert R. Röhrbein <gilbert.rolf.roehrbein@mail.inf.tu-dresden.de>
2010-08-18 00:43:59 +0200

    cleand Vec

--- Vec.cpp
+++ Vec.cpp
@@ -1,2 +1,14 @@
 #include "Vec.h"
 
+Vec::Vec(float32 x, float32 y) : b2Vec2(x, y) {}
+Vec::Vec(b2Vec2 vec) : b2Vec2(vec.x, vec.y) {}
+Vec::Vec(QPointF vec) : b2Vec2(vec.x(), vec.y()) {}
+Vec::Vec() : b2Vec2() {}
+
+QPointF Vec::q() { return QPointF(x, y); }
+
+Vec Vec::rand() {
+    qreal x = qrand()*2.0f*M_PI/RAND_MAX;
+    return Vec(qCos(x), qSin(x));
+}
+
--- Vec.h
+++ Vec.h
@@ -8,15 +8,12 @@
 class Vec : public b2Vec2
 {
 public:
-    Vec(float32 x, float32 y) : b2Vec2(x, y) {}
-    Vec(b2Vec2 vec) : b2Vec2(vec.x, vec.y) {}
-    Vec(QPointF vec) : b2Vec2(vec.x(), vec.y()) {}
-    Vec() : b2Vec2() {}
-    QPointF q() { return QPointF(x, y); }
-    static Vec rand() {
-        qreal x = qrand()*2.0f*M_PI/RAND_MAX;
-        return Vec(qCos(x), qSin(x));
-    }
+    Vec(float32 x, float32 y);
+    Vec(b2Vec2 vec);
+    Vec(QPointF vec);
+    Vec();
+    QPointF q();
+    static Vec rand();
 };
 
 #endif // VEC_H
