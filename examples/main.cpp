e2b6a037955b8d20589af8c150f0e3fe962141e7
Gilbert R. Röhrbein <gilbert.rolf.roehrbein@mail.inf.tu-dresden.de>
2010-09-01 00:50:25 +0200

    added signal emitting ContactListener

--- examples/main.cpp
+++ examples/main.cpp
@@ -4,6 +4,7 @@
 #include <View.h>
 
 #include <NaubPairSpammer.h>
+#include <ContactListener.h>
 
 int main(int argc, char *argv[])
 {
@@ -15,11 +16,14 @@ int main(int argc, char *argv[])
     view.show();
 
     QTimer timer;
-    timer.connect(timer, SIGNAL(timeout()), &naubino, SLOT(update()));
+    timer.connect(&timer, SIGNAL(timeout()), &naubino, SLOT(update()));
     timer.start(50);
 
     NaubPairSpammer spam(&naubino);
     spam.start(2000);
+
+    ContactListener conlistener;
+    naubino.world().SetContactListener(&conlistener);
 
     return app.exec();
 }
