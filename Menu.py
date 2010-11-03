6ffd2cf0c8f0c65a58800700aadd3a14a03575e3
Gilbert R. Röhrbein <payload@lavabit.com>
2010-11-03 01:31:31 +0100

    MenuButtons now outside of Menu

--- Menu.py
+++ Menu.py
@@ -7,65 +7,7 @@ from PyQt4.QtCore import *
 from PyQt4.QtGui import *
 from utils import *
 from Naub import Naub
-
-class Button(Cute):
-    pressed  = pyqtSignal(QGraphicsSceneMouseEvent)
-    released = pyqtSignal(QGraphicsSceneMouseEvent)
-    
-    @property
-    def pos(self):
-        pos = self.group.pos()
-        pos = pos.x(), pos.y()
-        return Vec2d(pos)
-    @pos.setter
-    def pos(self, pos):
-        pos = Vec2d(pos)
-        self.group.setPos(pos.x, pos.y)
-
-    @property
-    def radius(self): return self.__radius
-    @radius.setter
-    def radius(self, radius):
-        if radius != self.__radius:
-            self.__radius = radius
-            rect = QRectF(-radius, -radius, radius*2, radius*2)
-            self.elli.setRect(rect)
-
-    def __init__(self, naubino, layer = 1, rect = False):
-        self.naubino = naubino
-        self.group = group = QGraphicsItemGroup()
-        Cute.__init__(self, group)
-
-        self.set_clickable(group, True)
-        def mousePressEvent(event):
-            if not hasattr(event, "naubino_pointer"): return
-            if event.button() == Qt.LeftButton:
-                self.pressed.emit(event)
-
-        def mouseReleaseEvent(event):
-            if not hasattr(event, "naubino_pointer"): return
-            if event.button() == Qt.LeftButton:
-                self.released.emit(event)
-            
-        group.mousePressEvent = mousePressEvent
-        group.mouseReleaseEvent = mouseReleaseEvent
-        
-        group.setZValue(layer)
-
-        self.__radius = 0
-
-        self.elli = elli = QGraphicsRectItem(group) if rect else QGraphicsEllipseItem(group)
-        elli.setPen(QPen(Qt.NoPen))
-        elli.setBrush(QColor("black"))
-        elli.setRotation(5)
-
-        self.text = text = QGraphicsTextItem("???", group)
-        font = text.font()
-        font.setBold(True)
-        text.setFont(font)
-        text.setDefaultTextColor(QColor("white"))
-
-        naubino.add_cute(self)
+from MenuButtons import *
 
 class State(QState):
     def __init__(self, naubino, state):
@@ -108,37 +50,6 @@ class FailState(State):
 
     def onExit(self, event):
         print("exit fail")
-
-class MenuButton(Button):
-    def __init__(self, naubino, radius, fontsize, text, cw, ch, rect = False):
-        Button.__init__(self, naubino, rect = rect)
-
-        self.radius = radius
-
-        font = self.text.font()
-        font.setPixelSize(self.radius * fontsize)
-        self.text.setFont(font)
-        self.text.setPlainText(text)
-
-        rect = self.elli.boundingRect()
-        pos = Vec2d(rect.x(), rect.y())
-        pos += Vec2d(radius, radius)
-        rect = self.text.boundingRect()
-        w, h = rect.width(), rect.height()
-        pos -= Vec2d(cw*w, ch*h)
-        self.text.setPos(pos.x, pos.y)
-
-class PlayButton(MenuButton):
-    def __init__(self, naubino):
-        MenuButton.__init__(self, naubino, 15, 1.8, "▸", 0.43, 0.565)
-
-class TutorialButton(Button):
-    def __init__(self, naubino):
-        MenuButton.__init__(self, naubino, 15, 1.5, "¿", 0.55, 0.5)
-
-class HighscoreButton(Button):
-    def __init__(self, naubino):
-        MenuButton.__init__(self, naubino, 17, 1.8, "5", 0.53, 0.5, rect=True)
 
 class Menu(QObject):
     play = pyqtSignal()
