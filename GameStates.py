ec2ab9b362fb367edef1595789d1eba8831ee0c6
Gilbert R. Röhrbein <payload@lavabit.com>
2010-11-08 01:48:47 +0100

    ItemFader added for fading QGraphicsItem

--- GameStates.py
+++ GameStates.py
@@ -1,48 +1,76 @@
 from PyQt4.QtCore import (QStateMachine, QState, pyqtSignal, pyqtProperty,
-    QPropertyAnimation)
+    QPropertyAnimation, QObject)
 from PyQt4.QtGui import *
+
+class ItemFader(QObject):
+    @pyqtProperty(float)
+    def opacity(self): return self.item.opacity()
+    @opacity.setter
+    def opacity(self, x): self.item.setOpacity(x)
+    
+    def __init__(self, item):
+        super(ItemFader, self).__init__()
+        self.item = item
+
+        ani = self.fade_in_ani = QPropertyAnimation(self, "opacity")
+        def fade_in_finished():
+            pass
+        ani.finished.connect(fade_in_finished)
+
+        ani = self.fade_out_ani = QPropertyAnimation(self, "opacity")
+        def fade_out_finished():
+            self.item.setVisibile(False)
+        ani.finished.connect(fade_out_finished)
+
+    def fade_in(self, opacity = 1.0, duration = 1.0):
+        self.item.setVisible(True)
+        self.fade(opacity, duration)
+
+    def fade_out(self, opacity = 0.0, duration = 1.0):
+        self.fade(opacity, duration)
+
+    def fade(self, opacity, duration):
+        ani = self.fade_in_ani
+        ani.setEndValue(opacity)
+        ani.setDuration(duration * 1000)
+        ani.start()
 
 class State(QState):
     def __init__(self, scene, state):
-        QState.__init__(self, state)
+        super(QState, self).__init__(state)
         self.scene = scene
         self.naubino = scene.naubino
 
 class HighscoreState(State):
+    def __init__(self, scene, state):
+        super(HighscoreState, self).__init__(scene, state)
+        #self.highscore = Highscore()
+    
     def onEntry(self, event):
-        print("enter highscore")
+        #self.highscore.load()
+        pass
 
     def onExit(self, event):
         print("exit highscore")
 
 class StartState(State):
-    @pyqtProperty(float)
-    def splash_opacity(self): return self.splash.opacity()
-    @splash_opacity.setter
-    def splash_opacity(self, x): self.splash.setOpacity(x)
-    
     def __init__(self, scene, state):
         super(StartState, self).__init__(scene, state)
-        self.ani = None
+        self.splash = splash = QGraphicsPixmapItem()
+        self.fader = fader = ItemFader(splash)
+
         pixmap = QPixmap("splash.png")
-        self.splash = splash = QGraphicsPixmapItem(pixmap)
+        splash.setVisible(False)
+        splash.setPixmap(pixmap)
         splash.setOpacity(0)
         splash.setPos(-300, -200)
+        self.scene.add_item(self.splash)
     
     def onEntry(self, event):
-        self.scene.add_item(self.splash)
-        
-        ani = self.ani = QPropertyAnimation(self, "splash_opacity")
-        ani.setEndValue(1)
-        ani.setDuration(1000)
-        ani.start()
+        self.fader.fade_in()
 
     def onExit(self, event):
-        ani = self.ani = QPropertyAnimation(self, "splash_opacity")
-        ani.setEndValue(0)
-        ani.setDuration(1000)
-        ani.finished.connect(lambda: self.scene.remove_item(self.splash))
-        ani.start()
+        self.fader.fade_out()
 
 class PlayState(State):
     def onEntry(self, event):
