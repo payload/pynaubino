d06624d77e18c6d3fe121ca62abc477b733d8f82
Gilbert R. Röhrbein <payload@lavabit.com>
2010-11-08 15:39:23 +0100

    highscore is styled with html :D

--- GameStates.py
+++ GameStates.py
@@ -59,14 +59,44 @@ class HighscoreState(State):
         highscore = self.scene.highscore
         if not highscore: return
         score = highscore.load_score()
-        score = [[str(y) for y in x] for x in score]
-        score = ["\t".join(x) for x in score]
-        score = "\n".join(score)
-        self.table.setPlainText(score)
+        score = self.generate_highscore_html(score)
+        self.table.setHtml(score)
         self.fader.fade_in()
 
     def onExit(self, event):
         self.fader.fade_out()
+
+    def generate_highscore_html(self, score_table):
+        sizes = ["xx-large", "x-large", "large"]
+
+        def score_style(sizes):
+            if not sizes: return ""
+            size = sizes.pop(0)
+            
+            style = [
+                "font-size:{0}".format(size),
+                "vertical-align:bottom",
+                "padding-right:40px"]
+            return ";".join(style)
+
+        def name_style():
+            style = ["vertical-align:bottom"]
+            return ";".join(style)
+        
+        html = ""
+        html += "<table>"
+        for line in score_table:
+            score, name = line
+            html += '<tr>'
+            html += '<td style="{0}">'.format(score_style(sizes))
+            html += str(score)
+            html += '</td>'
+            html += '<td style="{0}">'.format(name_style())
+            html += name
+            html += '</td>'
+            html += '</tr>'
+        html += "</table>"
+        return html
 
 class StartState(State):
     def __init__(self, scene, state):
