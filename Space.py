71e2f7fc320f665614c309b3e7de44a49c574b17
Gilbert R. Röhrbein <payload@lavabit.com>
2010-11-15 16:23:43 +0100

    added check: dont remove a object twice - fixed a bug, but didnt tracked it down

--- Space.py
+++ Space.py
@@ -5,10 +5,15 @@ class Space(pymunk.Space):
         pymunk.Space.__init__(self)
         self.damping = 0.4
         self.set_default_collision_handler(None, None, self.collide, None)
+        self.__to_remove = set()
 
     def remove(self, *objs):
+        to_remove = self.__to_remove
         for obj in objs:
+            if obj in to_remove: continue
+            to_remove.add(obj)
             def callback(obj):
+                to_remove.remove(obj)
                 pymunk.Space.remove(self, obj)
             self.add_post_step_callback(callback, obj)
 
