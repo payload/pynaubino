eb1626649a6a71ca805e953c6756081621512631
Gilbert Röhrbein <payload@payload-bay.de>
2014-02-16 11:39:23 +0100

    Space.py removed redundant book keeping

--- src/Space.py
+++ src/Space.py
@@ -5,19 +5,13 @@ class Space(pymunk.Space):
         pymunk.Space.__init__(self)
         self.damping = 0.4
         self.set_default_collision_handler(None, None, self.collide, None)
-        self.__objs = set()
 
     def add(self, *objs):
-        myobjs = self.__objs
         for obj in objs:
-            myobjs.add(obj)
             pymunk.Space.add(self, obj)
 
     def remove(self, *objs):
-        myobjs = self.__objs
         for obj in objs:
-            if obj not in myobjs: continue
-            myobjs.remove(obj)
             pymunk.Space.remove(self, obj)
 
     def collide(self, space, arbiter, *args, **kwargs):
